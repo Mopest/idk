d94834691f8467f0fb9a5ce506b8ce9d8a0dddda
Mopest <95849585+Mopest@users.noreply.github.com>
2021-12-09 10:34:15 +0100

    dd
    dd

---
+++ function.js
@@ -0,0 +1,25 @@
+var x = document.getElementsByCID("flipcard.front");
+var x = document.getElementsById("flipcard.back");
+var x = document.getElementsById("button.black");
+var x = document.getElementsById("button.white");
+
+button.onclick = function(nr){
+	
+	if (nr===1){alert("eins")}
+	if (nr===2){}
+}
+
+function show(nr){
+	if (color===1){alert("test")}
+	if (color===2){}
+}
+}
+
+
+	
+
+	function changecolor(){
+document.querySelector('.boxblack').style.color='blue'
+	
+
+	
